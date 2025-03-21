83fcdfcbd84c4e7cc4535b891a77b8237bd4a2d2
marko <onlymarko25@gmail.com>
2025-03-21 07:53:57 +0700

    fixing the foundry format

--- src/VotingSystem.sol
+++ src/VotingSystem.sol
@@ -12,10 +12,17 @@ contract VotingSystem {
         uint256 maxVotes;
         uint256 duration;
     }
+
     uint256 ID = 0;
     mapping(uint256 => Poll) polls;
 
-    function createPoll(string calldata _name, string[] calldata _candidates, string calldata _description, uint256 _maxVotes, uint256 _duration) external {
+    function createPoll(
+        string calldata _name,
+        string[] calldata _candidates,
+        string calldata _description,
+        uint256 _maxVotes,
+        uint256 _duration
+    ) external {
         if (_candidates.length <= 0 || _candidates.length > 3) {
             revert InsufficientCandidates(_candidates);
         }
