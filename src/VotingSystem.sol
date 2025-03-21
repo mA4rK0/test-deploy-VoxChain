24af5ffffefa5d16aa4dca2d86f7f928fd0a1fbe
marko <onlymarko25@gmail.com>
2025-03-21 11:19:51 +0700

    checkpoint 2

--- src/VotingSystem.sol
+++ src/VotingSystem.sol
@@ -3,6 +3,21 @@ pragma solidity ^0.8.20;
 
 contract VotingSystem {
     error InsufficientCandidates(string[] _candidates);
+    error AlreadyVoted(address _voter);
+    error Ended(uint256 _duration);
+    error MaxVotesReached(uint256 _maxVotes);
+
+    event PollCreated(
+        address _creator,
+        string[] _candidates,
+        string _description,
+        uint256 _maxVotes,
+        uint256 _duration,
+        uint256 _startTime,
+        uint256 _totalVoters,
+        bool _isCompleted
+    );
+    event Voted(address _voter, string _candidate);
 
     struct Poll {
         address creator;
@@ -11,10 +26,15 @@ contract VotingSystem {
         string description;
         uint256 maxVotes;
         uint256 duration;
+        uint256 startTime;
+        uint256 totalVoters;
+        bool isCompleted;
     }
 
-    uint256 ID = 0;
-    mapping(uint256 => Poll) polls;
+    Poll poll;
+    string public winner;
+    mapping(string => uint256) public candidatesVotes;
+    mapping(address => bool) public hasVoted;
 
     function createPoll(
         string calldata _name,
@@ -23,11 +43,26 @@ contract VotingSystem {
         uint256 _maxVotes,
         uint256 _duration
     ) external {
-        if (_candidates.length <= 0 || _candidates.length > 3) {
-            revert InsufficientCandidates(_candidates);
+        if (_candidates.length <= 0 || _candidates.length > 3) revert InsufficientCandidates(_candidates);
+
+        emit PollCreated(
+            msg.sender, _candidates, _description, _maxVotes, _duration, block.timestamp + block.number, 0, false
+        );
+        for (uint256 i = 0; i < _candidates.length; i++) {
+            candidatesVotes[_candidates[i]] = 0;
         }
-        Poll memory poll = Poll(msg.sender, _name, _candidates, _description, _maxVotes, _duration);
-        ID++;
-        polls[ID] = poll;
+        poll = Poll(
+            msg.sender, _name, _candidates, _description, _maxVotes, _duration, block.timestamp + block.number, 0, false
+        );
+    }
+
+    function vote(string calldata _candidate) external {
+        if (block.timestamp - poll.startTime >= poll.duration) revert Ended(poll.duration);
+        if (poll.totalVoters >= poll.maxVotes) revert MaxVotesReached(poll.maxVotes);
+        if (hasVoted[msg.sender] == true) revert AlreadyVoted(msg.sender);
+
+        hasVoted[msg.sender] = true;
+        emit Voted(msg.sender, _candidate);
+        candidatesVotes[_candidate]++;
     }
 }
