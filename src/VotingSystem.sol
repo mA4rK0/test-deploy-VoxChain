d9837d948f9a7e24d025fd27888da2ce1b9322a3
marko <onlymarko25@gmail.com>
2025-03-21 18:01:44 +0700

    create a winner

--- src/VotingSystem.sol
+++ src/VotingSystem.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 
 contract VotingSystem {
@@ -6,6 +6,7 @@ contract VotingSystem {
     error AlreadyVoted(address _voter);
     error Ended(uint256 _duration);
     error MaxVotesReached(uint256 _maxVotes);
+    error NotDoneYet(uint256 _time);
 
     event PollCreated(
         address _creator,
@@ -48,7 +49,7 @@ contract VotingSystem {
         emit PollCreated(
             msg.sender, _candidates, _description, _maxVotes, _duration, block.timestamp + block.number, 0, false
         );
-        for (uint256 i = 0; i < _candidates.length; i++) {
+        for (uint8 i = 0; i < _candidates.length; i++) {
             candidatesVotes[_candidates[i]] = 0;
         }
         poll = Poll(
@@ -63,6 +64,27 @@ contract VotingSystem {
 
         hasVoted[msg.sender] = true;
         emit Voted(msg.sender, _candidate);
+        poll.totalVoters++;
         candidatesVotes[_candidate]++;
+    }
+
+    function chooseWinner() external {
+        if (block.timestamp - poll.startTime < poll.duration) revert NotDoneYet(poll.duration);
+        string memory win;
+
+        for (uint8 i = 0; i <= poll.candidates.length; i++) {
+            if (i == 0) {
+                win = poll.candidates[i];
+            } else {
+                if (
+                    candidatesVotes[poll.candidates[i]] > candidatesVotes[poll.candidates[i + 1]]
+                        && candidatesVotes[poll.candidates[i]] > candidatesVotes[poll.candidates[i - 1]]
+                ) {
+                    win = poll.candidates[i];
+                }
+            }
+        }
+
+        winner = win;
     }
 }
