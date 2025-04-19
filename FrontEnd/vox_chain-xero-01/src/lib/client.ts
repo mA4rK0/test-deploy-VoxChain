9916e9d154eddd3206b252694290ccefc34894cb
marko <onlymarko25@gmail.com>
2025-04-19 13:56:36 +0700

    refactor : delete unimportant file and change the contract address

--- FrontEnd/vox_chain-xero-01/src/lib/client.ts
+++ FrontEnd/vox_chain-xero-01/src/lib/client.ts
@@ -5,11 +5,9 @@ import { inAppWallet } from "thirdweb/wallets";
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!; // this will be used on the client
 const secretKey = process.env.THIRDWEB_SECRET_KEY!; // this will be used on the server-side
 
-export const CONTRACT_ADDRESS = "0x8267fB15Af08de6B5e618ADA64b3F25374E4B699";
+export const CONTRACT_ADDRESS = "0xC6731c047802A192AD2d44cc383455F12f32485B";
 
-export const client = createThirdwebClient(
-  secretKey ? { secretKey } : { clientId }
-);
+export const client = createThirdwebClient(secretKey ? { secretKey } : { clientId });
 
 export const contract = getContract({
   address: CONTRACT_ADDRESS,
